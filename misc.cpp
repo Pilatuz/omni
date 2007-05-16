07e993760ee1e9a3d36015c1f5b2a0ce316baf4f
d1mk4 <d1mk4@18c7913e-9330-0410-ad6e-b685ac691ac5>
2007-05-16 10:56:46 +0000

    git-svn-id: https://omni.svn.sourceforge.net/svnroot/omni@1 18c7913e-9330-0410-ad6e-b685ac691ac5

---
+++ misc.cpp
@@ -0,0 +1,98 @@
+//////////////////////////////////////////////////////////////////////////
+//		This material is provided "as is", with absolutely no warranty
+//	expressed or implied. Any use is at your own risk.
+//
+//		Permission to use or copy this software for any purpose is hereby
+//	granted without fee, provided the above notices are retained on all
+//	copies. Permission to modify the code and to distribute modified code
+//	is granted, provided the above notices are retained, and a notice that
+//	the code was modified is included with the above copyright notice.
+//////////////////////////////////////////////////////////////////////////
+/** @file
+@brief Implementation of the miscellaneous classes.
+
+		This file contains implementation of the omni::misc::Timer class.
+
+@author Sergey Polichnoy
+*/
+#include <omni/misc.hpp>
+
+#include <time.h>
+
+namespace omni
+{
+	// Timer class
+	namespace misc
+	{
+
+//////////////////////////////////////////////////////////////////////////
+/// @brief Default interval.
+/**
+		This constructor initializes the timer's interval by one second.
+*/
+Timer::Timer()
+	: m_ref_point(clock()),
+	  m_interval(CLOCKS_PER_SEC)
+{}
+
+
+//////////////////////////////////////////////////////////////////////////
+/// @brief Set timer's interval.
+/**
+		This constructor initializes the timer's interval by @a ms milliseconds.
+
+@param[in] ms Timer's interval in milliseconds.
+*/
+Timer::Timer(tick_type ms)
+	: m_ref_point(clock()),
+	  m_interval(ms * 1000/CLOCKS_PER_SEC)
+{}
+
+
+//////////////////////////////////////////////////////////////////////////
+/// @brief Get timer's interval.
+/**
+		This method returns the current timer's interval.
+
+@return Timer's interval in milliseconds.
+*/
+Timer::tick_type Timer::interval() const
+{
+	return m_interval * CLOCKS_PER_SEC/1000;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+/// @brief Check timer.
+/**
+		This method checks the timer's interval is elapsed.
+
+@return @b true if timer's interval is elapsed, otherwise @b false.
+*/
+bool Timer::elapsed() const
+{
+	const tick_type now = clock();
+	if (m_interval < (now - m_ref_point))
+	{
+		m_ref_point = now;
+		return true;
+	}
+
+	return false;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+/// @brief Force timer.
+/**
+		This method forces the timer, so next
+	elapsed() method call will return @b true.
+*/
+void Timer::force()
+{
+	const tick_type now = clock();
+	m_ref_point = now - (m_interval+1);
+}
+
+	} // Timer
+} // omni namespace
---
+++ misc.hpp
@@ -0,0 +1,82 @@
+//////////////////////////////////////////////////////////////////////////
+//		This material is provided "as is", with absolutely no warranty
+//	expressed or implied. Any use is at your own risk.
+//
+//		Permission to use or copy this software for any purpose is hereby
+//	granted without fee, provided the above notices are retained on all
+//	copies. Permission to modify the code and to distribute modified code
+//	is granted, provided the above notices are retained, and a notice that
+//	the code was modified is included with the above copyright notice.
+//////////////////////////////////////////////////////////////////////////
+/** @file
+@brief Interface of the miscellaneous classes.
+
+		This file contains interface of the omni::misc::Timer class.
+
+@author Sergey Polichnoy
+*/
+#ifndef __OMNI_MISC_HPP_
+#define __OMNI_MISC_HPP_
+
+#include <omni/defs.hpp>
+
+namespace omni
+{
+	namespace misc
+	{
+
+//////////////////////////////////////////////////////////////////////////
+/** @class Timer
+	@brief %Timer object.
+
+		The Timer class is used for keeping time intervals.
+
+		It may be used to perform some periodic actions.
+	For example to print simulation statistics each second:
+
+@code
+	Timer t(1000);
+
+	while (1)
+	{
+		// simulation...
+
+		if (t.elapsed())
+		{
+			// print statistics...
+		}
+	}
+@endcode
+
+		One timer's tick is equal to one millisecond.
+
+		To change timer's interval the following code can be used:
+
+@code
+	Timer t; // default interval - 1 second
+	t = Timer(2000); // 2 seconds
+	t = Timer(500);  // 1/2 second
+@endcode
+*/
+class Timer {
+public:
+	typedef long tick_type; ///< @brief Tick type.
+
+public:
+	Timer();
+	explicit Timer(tick_type ms);
+
+public:
+	tick_type interval() const;
+	bool elapsed() const;
+	void force();
+
+private:
+	mutable tick_type m_ref_point;
+	        tick_type m_interval;
+};
+
+	} // misc namespace
+} // omni namespace
+
+#endif // __OMNI_MISC_HPP_
