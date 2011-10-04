0ef5ea986319e552b6e816546b799ca58106ccf4
Sergey Polichnoy <pilatuz@gmail.com>
2011-10-04 15:20:38 +0400

    misc::Events splitted to two parts:
    - misc::Events manages the list of subscribers
    - misc::EventsDriver emits the events
    
    misc::Events supports CallbackPtr template parameter,
      so we can use some share_ptr (NOT TESTED yet)
    misc unit-test introduced
    
    see example in test/misc.cpp

--- src/omni/misc.hpp
+++ src/omni/misc.hpp
@@ -30,14 +30,13 @@ namespace omni
 	namespace misc
 	{
 
-//////////////////////////////////////////////////////////////////////////
-/** @class Timer
-	@brief %Timer object.
+///////////////////////////////////////////////////////////////////////////////
+/// @brief The passive timer.
+/**
+	The Timer class is used for keeping time intervals.
 
-		The Timer class is used for keeping time intervals.
-
-		It may be used to perform some periodic actions.
-	For example to print simulation statistics each second:
+	It may be used to perform some periodic actions.
+For example to print simulation statistics each second:
 
 @code
 	Timer t(1000);
@@ -53,17 +52,18 @@ namespace omni
 	}
 @endcode
 
-		One timer's tick is equal to one millisecond.
+	One timer's tick is equal to one millisecond.
 
-		To change timer's interval the following code can be used:
+	To change timer's interval the following code can be used:
 
 @code
-	Timer t; // default interval - 1 second
+	Timer t; // 1 second by default
 	t = Timer(2000); // 2 seconds
-	t = Timer(500);  // 1/2 second
+	t = Timer(500);  // half of second
 @endcode
 */
-class Timer {
+class Timer
+{
 public:
 	typedef long tick_type; ///< @brief Tick type.
 
@@ -88,37 +88,98 @@ private:
 	namespace misc
 	{
 
-//////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 /// @brief The Events template class.
-template<typename HANDLER, typename AL = std::allocator<HANDLER*> >
-class Events {
-	typedef std::vector<HANDLER*, AL> ListType;
+/**
+	This class is used to manage subscribers, i.e. insert or remove.
+To emit some events use EventsDriver class.
 
+	The Callback interface is used to provide a set of callback methods.
+
+@code
+class Document
+{
 public:
-	typedef Events<HANDLER, AL> this_type;
-	typedef HANDLER handler_type;
-
-public:
-
-//////////////////////////////////////////////////////////////////////////
-/// @brief Insert new events handler.
-	void insert(handler_type &handler)
+	struct MyEvents
 	{
-		m_handlers.push_back(&handler);
+		virtual void onChanging(Document *pDoc) = 0;
+		virtual void onChanged(Document *pDoc) = 0;
+	};
+
+	omni::misc::Events<MyEvents>& events()
+	{
+		return m_events;
+	}
+
+private:
+	omni::misc::EventsDriver<MyEvents> m_events;
+
+	void doChaning()
+	{
+		m_events.emit(&MyEvents::onChanging, this);
+	}
+
+	void doChanged()
+	{
+		m_events.emit(&MyEvents::onChanged, this);
+	}
+};
+
+@endcode
+
+@warning By default this class stores the pointers to the subscribers.
+	Be sure to remove subscriber reference before destroying.
+
+It is also possible to use shread pointers here.
+*/
+template<typename Callback, typename CallbackPtr = Callback*>
+class Events
+{
+public:
+	typedef Events<Callback> ThisType;             ///< @brief The this type.
+	typedef std::vector<CallbackPtr> CallbackList; ///< @brief The list of callbacks.
+
+public:
+
+	/// @brief The default constructor.
+	Events()
+	{}
+
+	/// @brief The trivial destructor.
+	~Events()
+	{}
+
+public:
+
+	/// @brief Insert new events subscriber.
+	/**
+	@param[in] subscriber The subscriber pointer.
+	*/
+	void insert(CallbackPtr const& subscriber)
+	{
+		m_subscribers.push_back(subscriber);
 	}
 
 
-//////////////////////////////////////////////////////////////////////////
-/// @brief Remove existing events handler.
-	bool remove(handler_type &handler)
-	{
-		typename ListType::reverse_iterator i = m_handlers.rbegin();
-		typename ListType::reverse_iterator ie = m_handlers.rend();
+	/// @brief Remove existing events subscriber.
+	/**
+		The last occurence will be removed.
 
+	@param[in] subscriber The subscriber pointer.
+	@return true if subscriber removed.
+	*/
+	bool remove(CallbackPtr const& subscriber)
+	{
+		typedef typename CallbackList::reverse_iterator RevIterator;
+		RevIterator i = m_subscribers.rbegin();
+		RevIterator const ie = m_subscribers.rend();
+
+		// we do not use std::find() here
+		// to minimize number of included header files
 		for (; i != ie; ++i)
-			if (*i == &handler)
+			if (*i == subscriber)
 		{
-			m_handlers.erase(i.base()-1);
+			m_subscribers.erase(i.base()-1);
 			return true;
 		}
 
@@ -127,73 +188,116 @@ public:
 
 public:
 
-//////////////////////////////////////////////////////////////////////////
-/// @brief Insert new events handler.
-	this_type& operator+=(handler_type &handler)
+	/// @brief Insert new events subscriber.
+	/**
+	@param[in] subscriber The subscriber pointer.
+	@return The this reference.
+	@see insert()
+	*/
+	ThisType& operator+=(CallbackPtr const& subscriber)
 	{
-		insert(handler);
+		insert(subscriber);
 		return *this;
 	}
 
-//////////////////////////////////////////////////////////////////////////
-/// @brief Remove existing events handler.
-	this_type& operator-=(handler_type &handler)
+
+	/// @brief Remove existing events subscriber.
+	/**
+	@param[in] subscriber The subscriber pointer.
+	@return The this reference.
+	@see remove()
+	*/
+	ThisType& operator-=(CallbackPtr const& subscriber)
 	{
 		remove(handler);
 		return *this;
 	}
 
+protected:
+
+	/// @brief The list of subscribers.
+	CallbackList m_subscribers;
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// @brief The EventsDriver template class.
+/**
+	This class is used to emit events.
+
+@see Events
+*/
+template<typename Callback, typename CallbackPtr = Callback*>
+class EventsDriver:
+	public Events<Callback, CallbackPtr>
+{
+	typedef typename CallbackList::const_iterator Iterator;
+
 public:
 
-//////////////////////////////////////////////////////////////////////////
-/// @brief Emit event (no arguments).
-	void emit(void (HANDLER::*f)()) const
+	/// @brief Emit event (no arguments).
+	/**
+	@param[in] f The callback method to call.
+	*/
+	void emit(void (Callback::*f)()) const
 	{
-		typename ListType::const_iterator i = m_handlers.begin();
-		typename ListType::const_iterator ie = m_handlers.end();
+		Iterator i = m_subscribers.begin();
+		Iterator const ie = m_subscribers.end();
 
 		for (; i != ie; ++i)
 			((*i)->*f)();
 	}
 
-//////////////////////////////////////////////////////////////////////////
-/// @brief Emit event (one argument).
-	template<typename A1>
-		void emit(void (HANDLER::*f)(A1), A1 a1) const
+
+	/// @brief Emit event (one argument).
+	/**
+	@param[in] f The callback method to call.
+	@param[in] a The first argument.
+	*/
+	template<typename A>
+	void emit(void (Callback::*f)(A), A a) const
 	{
-		typename ListType::const_iterator i = m_handlers.begin();
-		typename ListType::const_iterator ie = m_handlers.end();
+		Iterator i = m_subscribers.begin();
+		Iterator const ie = m_subscribers.end();
 
 		for (; i != ie; ++i)
-			((*i)->*f)(a1);
+			((*i)->*f)(a);
 	}
 
-//////////////////////////////////////////////////////////////////////////
-/// @brief Emit event (two arguments).
-	template<typename A1, typename A2>
-		void emit(void (HANDLER::*f)(A1, A2), A1 a1, A2 a2) const
+
+	/// @brief Emit event (two arguments).
+	/**
+	@param[in] f The callback method to call.
+	@param[in] a The first argument.
+	@param[in] b The second argument.
+	*/
+	template<typename A, typename B>
+	void emit(void (Callback::*f)(A,B), A a, B b) const
 	{
-		typename ListType::const_iterator i = m_handlers.begin();
-		typename ListType::const_iterator ie = m_handlers.end();
+		Iterator i = m_subscribers.begin();
+		Iterator const ie = m_subscribers.end();
 
 		for (; i != ie; ++i)
-			((*i)->*f)(a1, a2);
+			((*i)->*f)(a, b);
 	}
 
-//////////////////////////////////////////////////////////////////////////
-/// @brief Emit event (three arguments).
-	template<typename A1, typename A2, typename A3>
-		void emit(void (HANDLER::*f)(A1, A2, A3), A1 a1, A2 a2, A3 a3) const
+
+	/// @brief Emit event (three arguments).
+	/**
+	@param[in] f The callback method to call.
+	@param[in] a The first argument.
+	@param[in] b The second argument.
+	@param[in] c The third argument.
+	*/
+	template<typename A, typename B, typename C>
+	void emit(void (Callback::*f)(A,B,C), A a, B b, C c) const
 	{
-		typename ListType::const_iterator i = m_handlers.begin();
-		typename ListType::const_iterator ie = m_handlers.end();
+		Iterator i = m_subscribers.begin();
+		Iterator const ie = m_subscribers.end();
 
 		for (; i != ie; ++i)
-			((*i)->*f)(a1, a2, a3);
+			((*i)->*f)(a, b, c);
 	}
-
-private:
-	ListType m_handlers;
 };
 
 	} // Events
---
+++ src/test/misc.cpp
@@ -0,0 +1,133 @@
+//////////////////////////////////////////////////////////////////////////
+//		This material is provided "as is", with absolutely no warranty
+//	expressed or implied. Any use is at your own risk.
+//
+//		Permission to use or copy this software for any purpose is hereby
+//	granted without fee, provided the above notices are retained on all
+//	copies. Permission to modify the code and to distribute modified code
+//	is granted, provided the above notices are retained, and a notice that
+//	the code was modified is included with the above copyright notice.
+//
+//		http://omni.sourceforge.net
+//////////////////////////////////////////////////////////////////////////
+/** @file
+	@brief The unit-test of "misc.hpp".
+
+@author Sergey Polichnoy
+*/
+#include <omni/misc.hpp>
+#include <test/test.hpp>
+
+#include <iostream>
+
+namespace
+{
+
+
+class Document
+{
+public:
+	struct MyEvents
+	{
+		virtual void onChanging(Document *pDoc) = 0;
+		virtual void onChanged(Document *pDoc) = 0;
+		virtual void onNewTitle(Document *pDoc, const char *title) = 0;
+		virtual void onNewAuthor(Document *pDoc, const char *author, int flags) = 0;
+	};
+
+	omni::misc::Events<MyEvents>& events()
+	{
+		return m_events;
+	}
+
+private:
+	omni::misc::EventsDriver<MyEvents> m_events;
+
+public:
+	void doChaning()
+	{
+		m_events.emit(&MyEvents::onChanging, this);
+	}
+
+	void doChanged()
+	{
+		m_events.emit(&MyEvents::onChanged, this);
+	}
+
+	void doNewTitle(const char *title)
+	{
+		m_events.emit(&MyEvents::onNewTitle, this, title);
+	}
+
+	void doNewAuthor(const char *author, int flags)
+	{
+		m_events.emit(&MyEvents::onNewAuthor, this, author, flags);
+	}
+};
+
+class Handler:
+	public Document::MyEvents
+{
+public:
+	Handler()
+		: m_hits(0)
+	{}
+
+public:
+
+	virtual void onChanging(Document *pDoc) { pDoc; m_hits += 1000; }
+	virtual void onChanged(Document *pDoc)  { pDoc; m_hits += 100; }
+	virtual void onNewTitle(Document *pDoc, const char *title) { pDoc; title; m_hits += 10; }
+	virtual void onNewAuthor(Document *pDoc, const char *author, int flags) { pDoc; author; flags; m_hits += 1; }
+
+public:
+	int m_hits;
+};
+
+}
+
+// test function
+bool test_misc(std::ostream &os)
+{
+	os << " Events Test...";
+
+	Document doc;
+	Handler hh;
+	doc.events().insert(&hh);
+	doc.events().insert(&hh);
+	doc.events().insert(&hh);
+
+	for (int i = 0; i < 4; ++i)
+	{
+		doc.doChaning();
+		doc.doChanged();
+		doc.doNewTitle("title");
+		doc.doNewAuthor("author", 0);
+		doc.events().remove(&hh);
+	}
+
+	os << "done\n";
+
+	return hh.m_hits == 6666;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// unit test
+namespace
+{
+	// Test1 class
+	class Test1: public omni::test::UnitTest {
+		// test title
+		virtual const char* title() const
+		{
+			return "omni::misc";
+		}
+
+		// test function
+		virtual bool do_test(std::ostream &os) const
+		{
+			return test_misc(os);
+		}
+	} test1;
+} // namespace
